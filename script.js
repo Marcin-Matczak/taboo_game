caa05aaec32a604d450f3d79dc7f65188d3beea6
Marcin-Matczak <marcorn.88@gmail.com>
2023-03-16 21:25:19 +0100

    First shape of layout

---
+++ script.js
@@ -0,0 +1,15 @@
+const tabooCards = [
+  {
+    answer: 'goldfish',
+    tabooWords: ['bowl', 'pet', 'swim', 'orange', 'memory'],
+  },
+
+  {
+    answer: 'tart',
+    tabooWords: ['pop', 'taste', 'sour', 'pastry', 'fruit'],
+  },
+  {
+    answer: 'houdini',
+    tabooWords: ['magic', 'escape', 'dangerous', 'stunt', 'harry'],
+  },
+];
