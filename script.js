c995f75ead4836fb216feac85b93d494e5134a1f
Marcin-Matczak <marcorn.88@gmail.com>
2023-03-17 21:04:15 +0100

    The initial logic logic of creating cards

--- script.js
+++ script.js
@@ -13,3 +13,34 @@ const tabooCards = [
     tabooWords: ['magic', 'escape', 'dangerous', 'stunt', 'harry'],
   },
 ];
+
+const cardContainer = document.querySelector('.card');
+
+let randomCard = Math.floor(Math.random() * tabooCards.length);
+
+const currCard = tabooCards[randomCard];
+
+const cardRender = function (card) {
+  const { answer, tabooWords } = card;
+  const answerElement = `
+    <div class="card__answer">
+      <h2>${answer}</h2>
+    </div> 
+    <div class="card__tabooWords">
+      <ul>
+      </ul>
+    </div>
+  `;
+
+  cardContainer.insertAdjacentHTML('beforeend', answerElement);
+  const listContainer = document.querySelector('.card__tabooWords ul');
+  console.log(listContainer);
+
+  const taboWordsElement = tabooWords.forEach(word => {
+    const li = document.createElement('li');
+    li.innerText = word;
+    listContainer.appendChild(li);
+  });
+};
+
+cardRender(currCard);
