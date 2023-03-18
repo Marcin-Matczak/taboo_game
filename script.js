eb17e8223d66e858f25e5bfd595925c7162c7d86
Marcin-Matczak <marcorn.88@gmail.com>
2023-03-18 19:23:59 +0100

    Added functionality of  buttons

--- script.js
+++ script.js
@@ -14,17 +14,25 @@ const tabooCards = [
   },
 ];
 
+// Buttons
+
+const nextBtn = document.querySelector('.next');
+const previousBtn = document.querySelector('.previous');
+const drawBtn = document.querySelector('.random');
+
 const cardContainer = document.querySelector('.card');
+let randomNumber = Math.floor(Math.random() * tabooCards.length);
+let repeatedCard = [];
 
-let randomCard = Math.floor(Math.random() * tabooCards.length);
-
-const currCard = tabooCards[randomCard];
+let currCard;
 
 const cardRender = function (card) {
   const { answer, tabooWords } = card;
+  const tabooAnswer = answer.toUpperCase();
+
   const answerElement = `
     <div class="card__answer">
-      <h2>${answer}</h2>
+      <h2>${tabooAnswer}</h2>
     </div> 
     <div class="card__tabooWords">
       <ul>
@@ -34,13 +42,67 @@ const cardRender = function (card) {
 
   cardContainer.insertAdjacentHTML('beforeend', answerElement);
   const listContainer = document.querySelector('.card__tabooWords ul');
-  console.log(listContainer);
-
-  const taboWordsElement = tabooWords.forEach(word => {
+  tabooWords.forEach(word => {
+    word = word.at(0).toUpperCase() + word.slice(1);
     const li = document.createElement('li');
     li.innerText = word;
     listContainer.appendChild(li);
   });
 };
 
-cardRender(currCard);
+const checkCard = function () {
+  return repeatedCard.includes(randomNumber)
+    ? (randomNumber = Math.floor(Math.random() * tabooCards.length))
+    : console.log('All card were used!');
+};
+
+const randomCard = function () {
+  checkCard();
+  repeatedCard.push(randomNumber);
+  currCard = tabooCards[randomNumber];
+  cardRender(currCard);
+  console.log(repeatedCard);
+};
+
+randomCard();
+
+const nextCard = function () {
+  currIndex = tabooCards.indexOf(currCard);
+  let nextOne = {};
+  currCard === tabooCards.at(-1)
+    ? (nextOne = tabooCards[0])
+    : (nextOne = tabooCards[currIndex + 1]);
+  cardRender(nextOne);
+  currCard = nextOne;
+};
+
+const previousCard = function () {
+  currIndex = tabooCards.indexOf(currCard);
+  let nextOne = {};
+  currCard === tabooCards.at(0)
+    ? (nextOne = tabooCards.at(-1))
+    : (nextOne = tabooCards[currIndex - 1]);
+  cardRender(nextOne);
+  currCard = nextOne;
+};
+
+const clearCardContent = function () {
+  cardContainer.innerHTML = '';
+};
+
+nextBtn.addEventListener('click', function (e) {
+  clearCardContent();
+  nextCard();
+});
+
+previousBtn.addEventListener('click', function (e) {
+  clearCardContent();
+  previousCard();
+});
+
+drawBtn.addEventListener('click', function () {
+  clearCardContent();
+  randomCard();
+});
+
+console.log(currCard);
